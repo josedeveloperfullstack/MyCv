7760a27b210567677fb04f61d64e56ef1bf96c7e
Jose Alfredo Aguado <josealfredoaguadomesa@gmail.com>
2022-11-15 01:13:05 -0500

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+const aboutme = document.querySelector('#Aboutme')
+
+const button = document.querySelector('button')
+button.addEventListener('click', function(){
+    aboutme.innerHTML = "Excellet Person"
+    
+})
